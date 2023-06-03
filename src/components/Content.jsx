53d21bc71ff0e9339153ff947d484095ccefd358
GeorgeValle <jorgeguillermovalle@gmail.com>
2023-06-03 03:10:06 -0300

    (Feat): add Nav and Slogan components
    In home view add Slogan component, too nav bar

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+//import /*React,*/ {useState} from 'react';
+//import {faBookOpenReader, faHandsHolding, faUsers} from "@fortawesome/free-solid-svg-icons";
+import './Content.css';
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const Content = (/*{className}*/) => {
+  
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Content;
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,23 @@
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// 
+// import logo from '../assets/logo_white.svg';
+// import { faBook, faCompass, faCopyright, faHandshake } from "@fortawesome/free-solid-svg-icons";
+// import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+//import Content from "../components/Content.jsx";
+//import { useLayoutEffect } from 'react';
+import Slogan from '../components/Slogan.jsx'
+
+
+const HomeView = () => {
+
+
+
+    return (
+        <>
+<Slogan></Slogan>
+        </>
+    );
+
+}
+
+export default HomeView;
