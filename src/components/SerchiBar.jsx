a38ad58a789e537fb0e7e9e4242bcf299b9a5293
GeorgeValle <jorgeguillermovalle@gmail.com>
2023-06-27 01:22:01 -0300

    (fix) data fetching advance

--- src/components/SerchiBar.jsx
+++ src/components/SerchiBar.jsx
@@ -6,37 +6,46 @@ import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 
 const SerchiBar =()=>{
     const [input, setInput] = useState("");
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [term, setTerm] = useState("");  
+    // const [data, setData] = useState([]);
+    // const [loading, setLoading] = useState(true);
+
+
+    const {data, loading} =  UseFetch(term)
 
     const  handleChange =  (value)=>{
-            setInput(value);
-    const {data, loading} =  UseFetch(value)
-            
-
-            setData(data);
-            setLoading(loading);
+            setInput(value);       // setData(data);
+            // setLoading(loading);
     } 
+    const handleSubmit = (event)=>{
+        event.stopPropagation()
+        setTerm(input);
+    }
 
     return(
         <>
+
         <div className="input-wrapper">
             <FontAwesomeIcon id="search-icon" icon={faMagnifyingGlass} />
             <input 
             placeholder="Type article to search..." 
             value={input} 
             onChange={(e)=> handleChange(e.target.value)}/>
+            <input type="button" onClick={(e)=>handleSubmit(e)} value="Search"/>
             {/* <div>Search Bar</div>
             <div>search-bar result</div> */}
+        </div>
 
             <ul>
-                {loading && <li>Loading</li>}
-                {data?.map((dat) => (
+                {loading
+                ?<li>{<li>Loading</li>}</li>
+                :data?.map((dat) => (
                     <li key={dat._id}>{dat.title}</li>
-                ))}
+                ))
+                }
             </ul>
 
-        </div>
+        
         
             
             </>
--- src/components/UseFetch.js
+++ src/components/UseFetch.js
@@ -3,18 +3,28 @@ import axios from "axios";
 
 export  function UseFetch(searchTerm){
     const [data, setData] = useState(null);
+    const [message, setMessage]= useState(""); 
     const [loading, setLoading] = useState(false);
 
     useEffect(()=>{
-        setLoading(true);
-        axios.get(`https://wiki-ideas-back.fly.dev/topics/search/${searchTerm}`)
-        .then((response)=>response.json())
-        .then((datum)=>setData(datum))
-        .finally(()=>setLoading(false));
-
+        if(searchTerm){
+            setLoading(true);
+            // let baseURL="https://wiki-ideas-back.fly.dev/"
+            // let url = "topics/search/"
+            // let params=searchTerm
+            axios.get(`https://wiki-ideas-back.fly.dev/topics/search/${searchTerm}`)
+            // axios.get(baseURL,url,searchTerm)
+            // .then((response)=>response.json())
+            .then(function (response) {
+                setData(response.data),setMessage(response.message)
+            })
+            .catch ((error) => {
+            console.log (error)})
+            .finally(()=>setLoading(false));
+        }
         
-    },[]);
-    
-    return {data, loading}
+    },[searchTerm]);
+    console.log('have datum',data)
+    return {data, loading, message}
 }
 
