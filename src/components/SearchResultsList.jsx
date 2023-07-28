927ba1851ae48a7d97d3fde141aefb3351c08c60
GeorgeValle <jorgeguillermovalle@gmail.com>
2023-07-27 22:25:41 -0300

    refactor

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -4,12 +4,12 @@ import SearchResult from './SearchResult.jsx';
 
 const SearchResultsList =({results, setOpenModal, setOneResult}) => {
     //const entries = Object.entries(results);
-    console.log("Result in list:", results)
+    // console.log("Result in list:", results)
 
     const handleChange = (value)=>{
         
         setOneResult(value)
-        console.log("value one result:", value)
+        // console.log("value one result:", value)
         setOpenModal(true);
     }
     return (
